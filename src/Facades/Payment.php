5fa523a2d13352b75992cd57dc2f3ebf2984bb79
Sajjad Moghayyad <sajjadmgd77@gmail.com>
2022-08-13 14:38:52 +0430

    update payment facade

--- src/Facades/Payment.php
+++ src/Facades/Payment.php
@@ -3,11 +3,17 @@
 namespace Sajjadmgd\Zarinpal\Facades;
 
 use Illuminate\Support\Facades\Facade;
+use Sajjadmgd\Zarinpal\Models\Transaction;
 
 class Payment extends Facade
 {
     protected static function getFacadeAccessor()
     {
         return 'payment';
+    }
+
+    public static function getTransactions()
+    {
+        return Transaction::whereNotNull('id');
     }
 }
